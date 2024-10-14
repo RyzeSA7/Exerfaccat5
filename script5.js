4350ddf20a11519f0674a922c18af7c428093929
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 17:04:10 -0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,4 @@
+let salario_atual = parseInt(prompt("digite o salario atual"))
+let reajuste = parseInt(prompt("digite o reajuste do salario"))
+let salario_nv = salario_atual * reajuste
+alert ("o salario novo é:" + salario_nv)
